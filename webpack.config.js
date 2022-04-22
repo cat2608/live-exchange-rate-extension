f7fc73969a00b723f03c7a43299c20c28fb413b8
Cata <cat2608@gmail.com>
2022-04-22 08:23:49 +0100

    Add env file (#7)
    * chore: add prettier and update eslint
    
    * chore: Add ACCESS_KEY token for API
    
    Co-authored-by: sergio-imtlazarus <sergio@imtlazarus.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
+// webpack.config.js
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
   entry: './popup.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new Dotenv()
+  ]
 };
